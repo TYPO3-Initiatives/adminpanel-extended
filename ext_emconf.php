536bbc6363e63899c0966b2c3d684aefa942b15d
Susanne Moog <s.moog@neusta.de>
2018-10-03 17:36:58 +0200

    [TASK] Adjust TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,6 +11,7 @@ $EM_CONF['adminpanel_extended'] = [
 	'author_email' => '',
 	'constraints' => [
 		'depends' => [
+		    'typo3' => '9.5.0-9.5.99'
         ],
 		'conflicts' => [],
 		'suggests' => [],
