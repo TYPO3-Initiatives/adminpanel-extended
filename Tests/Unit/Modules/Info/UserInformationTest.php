84957cc9fd76e97a09fde3aafc5687bf722b90ec
kamiyang <github@jan-stockfisch.de>
2018-10-08 16:08:37 +0200

    [TASK] #84616 | Simple check on getContent() behavior

--- Tests/Unit/Modules/Info/UserInformationTest.php
+++ Tests/Unit/Modules/Info/UserInformationTest.php
@@ -3,10 +3,15 @@ declare(strict_types=1);
 
 namespace Psychomieze\AdminpanelExtended\Tests\Unit\Modules\Info;
 
+use Prophecy\Argument;
 use Psychomieze\AdminpanelExtended\Modules\Info\UserInformation;
 use TYPO3\CMS\Adminpanel\ModuleApi\AbstractSubModule;
+use TYPO3\CMS\Adminpanel\ModuleApi\ContentProviderInterface;
+use TYPO3\CMS\Adminpanel\ModuleApi\ModuleData;
 use TYPO3\CMS\Adminpanel\ModuleApi\ResourceProviderInterface;
 use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 /**
@@ -62,5 +67,44 @@ class UserInformationTest extends UnitTestCase
         static::assertInstanceOf(ResourceProviderInterface::class, $this->subject);
         static::assertSame($expectedJSResources, $this->subject->getJavaScriptFiles());
         static::assertSame($expectedCSSResources, $this->subject->getCssFiles());
+    }
+
+    /**
+     * @test
+     */
+    public function getContentShouldUsePassedModuleDataAndForwardTheseDataToTheView(): void
+    {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'] = 'ddmmyy';
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['hhmm'] = 'hhmm';
+
+        $viewProphecy = $this->prophesize(StandaloneView::class);
+        $viewProphecy->setTemplatePathAndFilename(Argument::any())->shouldBeCalled();
+        $viewProphecy->assignMultiple($this->getModuleDataFixture()->getArrayCopy())->shouldBeCalled();
+        $viewProphecy->render()->willReturn('some html');
+        GeneralUtility::addInstance(StandaloneView::class, $viewProphecy->reveal());
+
+        $this->subject->getContent($this->getModuleDataFixture());
+
+        static::assertInstanceOf(ContentProviderInterface::class, $this->subject);
+    }
+
+    /**
+     * @param bool $isPageBeingEdited
+     * @param int $onlineFrontendUsers
+     * @param int $onlineBackendUsers
+     * @return \TYPO3\CMS\Adminpanel\ModuleApi\ModuleData
+     */
+    protected function getModuleDataFixture(
+        bool $isPageBeingEdited = false,
+        int $onlineFrontendUsers = 0,
+        int $onlineBackendUsers = 0
+    ): ModuleData {
+        return new ModuleData([
+            'isPageBeingEdited' => $isPageBeingEdited,
+            'dataFormat' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['ddmmyy'],
+            'timeFormat' => $GLOBALS['TYPO3_CONF_VARS']['SYS']['hhmm'],
+            'onlineFrontendUsers' => $onlineFrontendUsers,
+            'onlineBackendUsers' => $onlineBackendUsers,
+        ]);
     }
 }
