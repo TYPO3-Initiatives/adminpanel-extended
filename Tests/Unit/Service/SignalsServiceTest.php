8cae3a58c5db1a2790d039c9a18f9712379f1a96
kamiyang <github@jan-stockfisch.de>
2019-06-14 10:38:12 +0200

    [TASK] Add unit test to increase code coverage

--- Tests/Unit/Service/SignalsServiceTest.php
+++ Tests/Unit/Service/SignalsServiceTest.php
@@ -5,12 +5,13 @@ namespace Psychomieze\AdminpanelExtended\Tests\Unit\Service;
 
 /*
  * This file is part of the TYPO3 Adminpanel Initiative.
- *t
+ *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
 
 use Psychomieze\AdminpanelExtended\Service\SignalsService;
+use Psychomieze\AdminpanelExtended\Tests\Unit\Fixtures\FirstDataProviderModuleFixture;
 use TYPO3\CMS\Core\Log\LogLevel;
 use TYPO3\CMS\Core\Log\LogRecord;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
@@ -36,5 +37,28 @@ class SignalsServiceTest extends UnitTestCase
         $result = $signalsService->getSignalDataFromLogRecord($record);
 
         self::assertSame($signalArguments, $result);
+    }
+
+    /**
+     * @test
+     */
+    public function getSignalDataFromLogRecordReturnsClassNameIfLogRecordContainsObjects(): void
+    {
+        $signalArguments = ['foo', 'baa' => new FirstDataProviderModuleFixture()];
+        $record = new LogRecord(
+            'TYPO3.CMS.Extbase.SignalSlot.Dispatcher',
+            LogLevel::DEBUG,
+            '',
+            [
+                'signalArguments' => $signalArguments,
+                'foo' => 'bar'
+            ]
+        );
+
+        $signalsService = new SignalsService();
+        $result = $signalsService->getSignalDataFromLogRecord($record);
+
+        $expected = ['foo', 'baa' => FirstDataProviderModuleFixture::class];
+        self::assertSame($expected, $result);
     }
 }
